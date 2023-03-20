1cba2557cdd492973fb022025474578c9e8482a7
Miguel Acosta <macosta@hitachisolutions.com>
2023-03-20 12:24:08 -0400

    revert versioning scheme

--- pack_clean_buildTarget.bat
+++ pack_clean_buildTarget.bat
@@ -7,11 +7,11 @@ rem 1. Pack the HitachiQA project
 dotnet pack HitachiQA\HitachiQA.csproj
 
 taskkill /IM chromedriver.exe /F
-cd "C:\Users\macosta\source\repos\Hitachi-SolutionsQA\Demo\Demo"
+cd "C:\Users\macosta\source\repos\Demo\Demo"
 dotnet clean Demo.csproj
 
-rem 2. Remove version 1.0.0 of the hitachiqa package from the .nuget folder
-del /Q "C:\Users\macosta\.nuget\packages\hitachiqa\1.0.0\*.*"
+rem 2. Remove version 1.0.1 of the hitachiqa package from the .nuget folder
+del /Q "C:\Users\macosta\.nuget\packages\hitachiqa\1.0.1\*.*"
 
 rem 3. Clean and build the Blackbird.AutomatedTesting.SOTests project
 dotnet build Demo.csproj
