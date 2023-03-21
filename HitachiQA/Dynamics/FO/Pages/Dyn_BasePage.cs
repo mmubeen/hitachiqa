3b4e5dd2704306f3aec61e1f3e38f9180dfece04
Alex Caldarera <acaldarera@hitachisolutions.com>
2023-03-21 12:24:21 -0400

    ..

--- HitachiQA/Dynamics/FO/Pages/Dyn_BasePage.cs
+++ HitachiQA/Dynamics/FO/Pages/Dyn_BasePage.cs
@@ -33,7 +33,7 @@ namespace HitachiQA.Dynamics.FO.Pages
 
         public void SetGridQuickFilterValue(string filterByColumn, string Criteria)
         {
-            Element($"//input[@name='GridFilter_Input']").SetFieldValue(Criteria);
+            Element($"//input[@name='GridFilter_Input']").setText(Criteria);
             Element($"//li[contains(@class, 'quickFilter')][descendant::*[text()='{filterByColumn}']][descendant::*[text()='{Criteria}']]").Click();
         }
     }
