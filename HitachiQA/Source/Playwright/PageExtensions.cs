5718b8240e6ea957a3ac349fad01e5632bb3202f
Miguel Acosta <macosta@hitachisolutions.com>
2023-05-04 14:58:21 -0400

    refactoring runtime field detection

--- HitachiQA/Source/Playwright/PageExtensions.cs
+++ HitachiQA/Source/Playwright/PageExtensions.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.Playwright;
+using Polly;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,22 +13,52 @@ namespace HitachiQA.Playwright
     {
         public static ILocator GetField(this IPage page, string indentifier)
         {
-            var tasks = new Dictionary<string, Task<int>>();
+            var tasks = new Dictionary<string, Task<bool>>();
+            var ct = new CancellationTokenSource();
+            //invoking all trials
             foreach (var trial in KnownFieldXPaths)
             {
                 var identifier = $"xpath={trial.Replace("{input}", indentifier)} /self::*[not(contains(@style,'display: none'))]";
-                tasks.Add(identifier, page.Locator(identifier).CountAsync());
-            }
-            var result = Task.WhenAll(tasks.Values).Result;
+                var field = page.Locator(identifier);
+                
+                //adding running task
+                tasks.Add(identifier, InvokeTrial(page, field, ct.Token));
+                
 
-            if (result.Any(it => it > 0))
+            }
+            
+            var completed = Task.WhenAny(tasks.Values).Result;
+                        
+            if (completed.Result== true)
             {
-                var identifier = tasks.First(it => it.Value.Result > 0).Key;
-                return page.Locator(identifier);
+                var identifierFound = tasks.First(it => it.Value.IsCompletedSuccessfully).Key;
+                ct.Cancel();
+                return page.Locator(identifierFound);
             }
             throw new Exception($"Not found in UI: {indentifier}");
 
 
+        }
+        [DebuggerHidden]
+        private static Task<bool> InvokeTrial(IPage page, ILocator fieldTrial, CancellationToken ct)
+        {
+            var retry = Policy.HandleResult<int>(0).WaitAndRetry(5 * 30, _ => TimeSpan.FromMilliseconds(200));
+            
+            var task = new Task<bool>(() => {
+
+                var count =  retry.Execute(() =>
+                {
+                    if(ct.IsCancellationRequested)
+                    {
+                        return -1;
+                    }
+                    return fieldTrial.CountAsync().Result;
+                });
+                return count != 0;
+                
+            });
+            task.Start();
+            return task??throw new Exception("error invoking trial, task was null");
         }
 
         public static List<string> KnownFieldXPaths = new List<string>()
