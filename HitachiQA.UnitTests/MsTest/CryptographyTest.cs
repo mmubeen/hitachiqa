2f4c1bb61b9c0a70c56ed392ed7023532ba519e0
Alex Caldarera <acaldarera@hitachisolutions.com>
2023-08-16 15:27:22 -0400

    Initial MSTest Unit Test set up and Cryptography unit tests

---
+++ HitachiQA.UnitTests/MsTest/CryptographyTest.cs
@@ -0,0 +1,24 @@
+﻿using HitachiQA.Helpers;
+
+namespace HitachiQA.UnitTests
+{
+    [TestClass]
+    public class CryptographyTest
+    {
+        [TestMethod]
+        public void IsStringEncrypted()
+        {
+            string theString = "test12";
+            string encrypt = Cryptography.Encrypt(theString);
+            encrypt.Should().Be("IvkLxQ7k+mp5OVoKenFqJw==");
+        }
+
+        [TestMethod]
+        public void IsStringDecrypted()
+        {
+            string encrypt = "IvkLxQ7k+mp5OVoKenFqJw==";
+            string decrypted = Cryptography.Decrypt(encrypt);
+            decrypted.Should().Be("test12");
+        } 
+    }
+}
