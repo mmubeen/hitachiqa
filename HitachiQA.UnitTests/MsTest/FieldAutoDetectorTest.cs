ba9c252ac6fa1f1b37d2baf2f48779adae133273
Alex Caldarera <acaldarera@hitachisolutions.com>
2023-08-17 09:42:38 -0400

    ..

--- HitachiQA.UnitTests/MsTest/FieldAutoDetectorTest.cs
+++ HitachiQA.UnitTests/MsTest/FieldAutoDetectorTest.cs
@@ -5,11 +5,25 @@ namespace HitachiQA.UnitTests
     public class FieldAutoDetectorTest
     {
         [TestMethod]
-        public void IsParsedToBool()
+        public void IsParsedToBoolTrue()
         {
-            var detector = FieldAutoDetector.parseStrIntoBool("true");
-            detector.Should().BeTrue();
+            string[] strings = { "1", "yes", "true", "check" };
+            foreach (var thing in strings)
+            {
+                var detector = FieldAutoDetector.parseStrIntoBool(thing);
+                detector.Should().BeTrue();
+            }
+        }
 
+        [TestMethod]
+        public void IsParsedToBoolFalse()
+        {
+            string[] strings = { "0", "no", "false", "uncheck" };
+            foreach (var thing in strings)
+            {
+                var detector = FieldAutoDetector.parseStrIntoBool(thing);
+                detector.Should().BeFalse();
+            }
         }
     }
 }
