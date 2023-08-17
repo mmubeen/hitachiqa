b26ebbdf978ecf6d6a7bd7002bd723610b716529
Alex Caldarera <acaldarera@hitachisolutions.com>
2023-08-17 08:34:25 -0400

    ..

---
+++ HitachiQA.UnitTests/MsTest/FieldAutoDetectorTest.cs
@@ -0,0 +1,15 @@
+﻿
+namespace HitachiQA.UnitTests
+{
+    [TestClass]
+    public class FieldAutoDetectorTest
+    {
+        [TestMethod]
+        public void IsParsedToBool()
+        {
+            var detector = FieldAutoDetector.parseStrIntoBool("true");
+            detector.Should().BeTrue();
+
+        }
+    }
+}
