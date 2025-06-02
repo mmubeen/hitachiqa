71fcadc6a25b5a97c81b3b07a0e8a8a476346282
bromero <bromero@hitachisolutions.com>
2025-06-02 11:58:31 -0600

    build automation

---
+++ ExpeditorsBuildAutomation/ExtensionMethods.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json.Linq;
+
+namespace ExpeditorsBuildAutomation
+{
+    internal static class ExtensionMethods
+    {
+        public async static Task<JObject> ParseIntoJObjectAsync(this HttpResponseMessage message)
+        {
+            var rawStr = await message.Content.ReadAsStringAsync();
+            return JObject.Parse(rawStr);
+        }
+    }
+}
---
+++ ExpeditorsBuildAutomation/ImplicitUsings.cs
@@ -0,0 +1,4 @@
+﻿global using FluentAssertions;
+global using Microsoft.VisualStudio.TestTools.UnitTesting;
+global using TechTalk.SpecFlow;
+global using HitachiQA;
