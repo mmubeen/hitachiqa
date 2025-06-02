95267edb6ada61c87950bca9222e47bac1db0a2b
bromero <bromero@hitachisolutions.com>
2025-06-02 11:59:40 -0600

    build automation

---
+++ ExpeditorsBuildAutomation/Hooks/HttpClientHooks.cs
@@ -0,0 +1,44 @@
+﻿using BoDi;
+using ExpeditorsBuildAutomation.Client.HttpClients;
+using HitachiQA;
+using HitachiQA.Source.Hooks.HttpClientExtras;
+using HitachiQA.Source.HttpClients;
+using Microsoft.Extensions.Configuration;
+using System.Net.Http.Headers;
+
+namespace ExpeditorsBuildAutomation.Hooks
+{
+    [Binding]
+    public class HttpClientHooks
+    {
+        private readonly IConfiguration config;
+
+        public HttpClientHooks(IConfiguration config)
+        {
+            this.config = config;
+        }
+
+        [BeforeFeature]
+        public static void InitializeClients(
+            IObjectContainer ioc, 
+            AuthorizationClient ac,
+            IConfiguration config)
+        {
+            var socketHander = new SocketsHttpHandler()
+            {
+                MaxConnectionsPerServer = 10
+            };
+            var logging = new HttpLoggingHandler(socketHander);
+            var auth = new HttpAuthHandler(logging, ac, config);
+            var retry = new HttpRetryHandler(auth);
+            var client = new HttpClient(retry);
+            var host = config.GetVariable("SERVER_HOST");
+            client.BaseAddress = new Uri(host);
+            client.Timeout = TimeSpan.FromSeconds(120);
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("*/*"));
+            var fabricsClient = new FabricsClient(client);
+            ioc.RegisterInstanceAs(fabricsClient);
+
+        }
+    }
+}
---
+++ ExpeditorsBuildAutomation/StepDefinitions/HealthCheckStepDefinitions.cs
@@ -0,0 +1,31 @@
+﻿using ExpeditorsBuildAutomation.Client.HttpClients;
+using ExpeditorsBuildAutomation.Client.Orchestrator;
+using HitachiQA;
+using Newtonsoft.Json.Linq;
+
+namespace ExpeditorsBuildAutomation.StepDefinitions;
+
+[Binding]
+public class HealthCheckStepDefinitions
+{
+    private readonly NoteBookOrchestrator _noteBookOrchestrator;
+
+    private JArray Notebooks { get; set; }
+
+    public HealthCheckStepDefinitions(NoteBookOrchestrator noteBookOrchestrator)
+    {
+        _noteBookOrchestrator = noteBookOrchestrator;
+    }
+
+    [When(@"user gets Notebooks from fabrics")]
+    public async Task WhenUserGetsNotebooksFromFabrics()
+    {
+        Notebooks = await _noteBookOrchestrator.GetNotebooks();
+    }
+
+    [Then(@"Notebooks should come back")]
+    public void ThenNotebooksShouldComeBack()
+    {
+        Log.Info(Notebooks[0]);
+    }
+}
