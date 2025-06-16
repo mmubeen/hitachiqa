b4b2c7d80720f94044864f9b1ddf66c34a86f8ac
bromero <bromero@hitachisolutions.com>
2025-06-16 10:43:49 -0600

    update test

---
+++ HitachiQA.Fabrics.Test/Orchestrators/LakehouseOrchestrator.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Fabric.Api;
+using Microsoft.Fabric.Api.Lakehouse.Models;
+
+namespace HitachiQA.Fabrics.Test.Orchestrators;
+
+public class LakehouseOrchestrator
+{
+    private readonly FabricClient fabricClient;
+    private readonly IConfiguration _config;
+
+    public LakehouseOrchestrator(FabricClient fabricClient, IConfiguration config)
+    {
+        this.fabricClient = fabricClient;
+        _config = config;
+    }
+
+    public async Task<IEnumerable<Lakehouse>> GetLakehousesAsync()
+    {
+        var workspace = Guid.Parse(_config.GetVariable("WORKSPACE_ID"));
+        var res = fabricClient.Lakehouse.Items.ListLakehouses(workspace);
+        var result = new List<Lakehouse>();
+        foreach (var lakehouse in res)
+        {
+            result.Add(lakehouse);
+        }
+        return await Task.FromResult(result);
+    }
+}
--- HitachiQA.Fabrics.Test/StepDefinitions/HealthCheckStepDefinitions.cs
+++ HitachiQA.Fabrics.Test/StepDefinitions/HealthCheckStepDefinitions.cs
@@ -1,6 +1,8 @@
 using HitachiQA.Fabrics.Test.Orchestrators;
 using Microsoft.Fabric.Api.DataPipeline.Models;
+using Microsoft.Fabric.Api.Lakehouse.Models;
 using Microsoft.Fabric.Api.Notebook.Models;
+using FabricEnvironment = Microsoft.Fabric.Api.Environment.Models.Environment;
 
 namespace HitachiQA.Fabrics.Test.StepDefinitions;
 
@@ -9,16 +11,25 @@ public class HealthCheckStepDefinitions
 {
     private readonly NoteBookOrchestrator _noteBookOrchestrator;
     private readonly DataPipelineOrchestrator _dataPipelineOrchestrator;
+    private readonly LakehouseOrchestrator _lakehouseOrchestrator;
+    private readonly EnvironmentOrchestrator _environmentOrchestrator;
 
     private IEnumerable<Notebook> Notebooks { get; set; }
     private IEnumerable<DataPipeline> DataPipelines { get; set; }
+    private IEnumerable<Lakehouse> Lakehouses { get; set; }
+    private IEnumerable<FabricEnvironment> Environments { get; set; }
+
 
     public HealthCheckStepDefinitions(
         NoteBookOrchestrator noteBookOrchestrator,
-        DataPipelineOrchestrator dataPipelineOrchestrator)
+        DataPipelineOrchestrator dataPipelineOrchestrator,
+        LakehouseOrchestrator lakehouseOrchestrator,
+        EnvironmentOrchestrator environmentOrchestrator)
     {
         _noteBookOrchestrator = noteBookOrchestrator;
         _dataPipelineOrchestrator = dataPipelineOrchestrator;
+        _lakehouseOrchestrator = lakehouseOrchestrator;
+        _environmentOrchestrator = environmentOrchestrator;
     }
 
     [When(@"user gets Notebooks from fabrics")]
@@ -31,6 +42,8 @@ public class HealthCheckStepDefinitions
         // get current state in fabrics
         Notebooks = await _noteBookOrchestrator.GetNotebooksAsync();
         DataPipelines = await _dataPipelineOrchestrator.GetDataPipelinesAsync();
+        Lakehouses = await _lakehouseOrchestrator.GetLakehousesAsync();
+        Environments = await _environmentOrchestrator.GetEnvironmentsAsync();
 
         //assert fabrics matches the expected in github
     }
@@ -49,5 +62,14 @@ public class HealthCheckStepDefinitions
         {
             Log.Info("DataPipeline name: " + item.DisplayName);
         }
+
+        Log.Info($"Lakehouse count: {Lakehouses.Count()}");
+        foreach (var item in Lakehouses)
+            Log.Info($"Lakehouse name: {item.DisplayName}");
+
+        Log.Info($"Environment count: {Environments.Count()}");
+        foreach (var item in Environments)
+            Log.Info($"Environment name: {item.DisplayName}");
+
     }
 }
